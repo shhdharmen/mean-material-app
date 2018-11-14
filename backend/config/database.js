7347a56bcdb2bd76052de78b6fe7641caa1bdddb
dharmen.s <dharmen.s@milestoneinternet.info>
2018-11-14 10:58:52 +0530

    db name changed, package file updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 // 27017 is the default port number.
 module.exports = {
     admin : 'mongodb://myUserAdmin:abc123@localhost:27017/admin',
-    tasks: 'mongodb://localhost:27017/kp-spec-app-db',
+    tasks: 'mongodb://localhost:27017/mean-material-app-db',
     users: 'mongodb://localhost:27017/users'
 }
